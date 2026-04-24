298b35c19edbf46dc6b7b925524d9d97399b1b75
IntFxZen <86683282+IntFxZen@users.noreply.github.com>
2026-04-24 15:35:01 +0300

    :APP Part_1 setup Koin DI and application bootstrap (#63)
    * chore: setup Koin DI and application bootstrap
    
    * fix: remove duplicate icons alias and restore testOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/itlab/notes/NotesApplication.kt
@@ -0,0 +1,16 @@
+package com.itlab.notes
+
+import android.app.Application
+import com.itlab.notes.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class NotesApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@NotesApplication)
+            modules(appModule)
+        }
+    }
+}
